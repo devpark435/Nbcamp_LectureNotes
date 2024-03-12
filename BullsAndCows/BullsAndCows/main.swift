129e108ed2b77996a974c9d185c3a380d3c87ef7
devpark435 <devpark435@gmail.com>
2024-03-12 16:41:45 +0900

    feat #1 - 랜덤 3자리 숫자 생성 및 출력

--- BullsAndCows/BullsAndCows/main.swift
+++ BullsAndCows/BullsAndCows/main.swift
@@ -6,6 +6,21 @@
 //
 
 import Foundation
+// MARK: -  야구 게임 클래스 호출
+let game = BaseballGame()
+game.start()
 
-print("Hello, World!")
+// MARK: - 야구 게임 클래스
+class BaseballGame {
+    // 게임 시작 함수
+    func start() {
+        let answer = makeAnswer()
+        print("정답: \(answer)")
+    }
+    // 정답 생성 함수
+    func makeAnswer() -> Int {
+        // 1~9 랜덤 3자리 숫자
+        return Int.random(in: 111...999)
+    }
+}
 
