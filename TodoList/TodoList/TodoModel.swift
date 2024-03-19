7730e045e14b4b59100d7e6c8635a73d10e7a47c
devpark435 <devpark435@gmail.com>
2024-03-19 15:39:14 +0900

    feat #3 - add todo struct

---
+++ TodoList/TodoList/TodoModel.swift
@@ -0,0 +1,13 @@
+//
+//  TodoModel.swift
+//  TodoList
+//
+//  Created by 박현렬 on 3/19/24.
+//
+
+import Foundation
+struct Todo {
+    var id: Int
+    var title: String
+    var isCompleted: Bool
+}
