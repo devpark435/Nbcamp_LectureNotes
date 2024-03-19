01dff44be93aa48e7a61199173b3975b021ed93f
devpark435 <devpark435@gmail.com>
2024-03-19 16:51:37 +0900

    feat #3 - AddTodo Tapped Event

--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -32,6 +32,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Do any additional setup after loading the view.
         title = "Todo List"
         setLayout()
+        addTarget()
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TodoCell")
@@ -42,6 +43,36 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func setLayout(){
         addButton.setTitle("Add Todo", for: .normal)
     }
-    
+    func addTarget(){
+        addButton.addTarget(self, action: #selector(addTodoTap), for: .touchUpInside)
+    }
+    @objc func addTodoTap() {
+        let alertController = UIAlertController(title: "할 일 추가", message: "할 일을 입력하세요", preferredStyle: .alert)
+        
+        alertController.addTextField { textField in
+            textField.placeholder = "할 일"
+        }
+        
+        let addAction = UIAlertAction(title: "추가", style: .default) { [weak self] _ in
+            guard let textField = alertController.textFields?.first,
+                  let newTodoTitle = textField.text,
+                  !newTodoTitle.isEmpty else {
+                // 입력된 내용이 없을 경우 처리
+                return
+            }
+            
+            // 입력된 내용을 처리하는 로직 추가
+            let newTodo = Todo(id: (self?.todos.count ?? 0) + 1, title: newTodoTitle, isCompleted: false)
+            self?.todos.append(newTodo)
+            self?.tableView.reloadData()
+        }
+        alertController.addAction(addAction)
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+
 }
 
