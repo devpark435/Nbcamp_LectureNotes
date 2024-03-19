7dcc417bcf9704f30325850e6650c44764c02ca4
devpark435 <devpark435@gmail.com>
2024-03-19 22:53:23 +0900

    feat #3 - add switch action

--- TodoList/TodoList/TodoCell.swift
+++ TodoList/TodoList/TodoCell.swift
@@ -5,4 +5,18 @@
 //  Created by 박현렬 on 3/19/24.
 //
 
-import Foundation
+import UIKit
+class TodoCell: UITableViewCell {
+    @IBOutlet weak var switchButton: UISwitch!
+    @IBOutlet weak var titleLabel: UILabel!
+    var todo: Todo?
+    @IBAction func switchValueChanged(_ sender: UISwitch) {
+            todo?.isCompleted = sender.isOn // Todo 모델 업데이트
+            // 스위치 상태에 따라 텍스트 색상 변경
+            if sender.isOn {
+                titleLabel.textColor = .blue
+            } else {
+                titleLabel.textColor = .black
+            }
+        }
+}
