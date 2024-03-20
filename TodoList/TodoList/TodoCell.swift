638a5a0f02d3d3913549c8ef8680bfc69bac9249
devpark435 <devpark435@gmail.com>
2024-03-20 17:01:35 +0900

    feat #3 - 스위치 동작에 따른 uiview 색상 변경

--- TodoList/TodoList/TodoCell.swift
+++ TodoList/TodoList/TodoCell.swift
@@ -6,20 +6,30 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+
 class TodoCell: UITableViewCell {
     @IBOutlet weak var switchButton: UISwitch!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryColor: UIView!
     var todo: Todo?
+    
+    
     @IBAction func switchValueChanged(_ sender: UISwitch) {
-            todo?.isCompleted = sender.isOn // Todo 모델 업데이트
-            // 스위치 상태에 따라 텍스트 색상 변경
-            if sender.isOn {
-                titleLabel.textColor = .gray
-                todo?.isCompleted = true
-                titleLabel.text =  "\(todo?.title ?? "") 완료👍"
-            } else {
-                titleLabel.textColor = .black
-                todo?.isCompleted = false
-                titleLabel.text =  "\(todo?.title ?? "")"}
+        todo?.isCompleted = sender.isOn // Todo 모델 업데이트
+        // 스위치 상태에 따라 텍스트 색상 변경
+        if sender.isOn {
+            titleLabel.textColor = .gray
+            todo?.isCompleted = true
+            titleLabel.text =  "\(todo?.title ?? "") 완료👍"
+            categoryColor.backgroundColor = .green
+        } else {
+            titleLabel.textColor = .black
+            todo?.isCompleted = false
+            titleLabel.text =  "\(todo?.title ?? "")"
+            categoryColor.backgroundColor = .white
         }
+        
+    }
 }
